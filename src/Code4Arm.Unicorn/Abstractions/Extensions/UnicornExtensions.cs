ab264f3da6c6a7c89267c920ddb2446a1a0a6c9a
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2022-05-12 11:59:09 +0200

    Add and document extension methods

--- src/Code4Arm.Unicorn/Abstractions/Extensions/UnicornExtensions.cs
+++ src/Code4Arm.Unicorn/Abstractions/Extensions/UnicornExtensions.cs
@@ -11,81 +11,87 @@ namespace Code4Arm.Unicorn.Abstractions.Extensions;
 public static class UnicornExtensions
 {
     /// <summary>
-    /// Reads a zero-terminated 'C-string' from the virtual memory. 
+    /// Reads a null-terminated 'C-string' from the emulated memory. 
     /// </summary>
     /// <param name="unicorn">The Unicorn instance.</param>
     /// <param name="address">The address of the string's beginning.</param>
-    /// <param name="stringBuilder">A <see cref="StringBuilder"/> instance to use when appending retrieved chunks of memory. If null, a new instance is created.</param>
+    /// <param name="maxLength">The maximum number of bytes to read before an exception is thrown.</param>
+    /// <param name="maxAddress">The maximum address where a read can start. Defaults to <see cref="ulong.MaxValue"/>.</param>
+    /// <param name="encoding">The encoding to decode the memory bytes with. If null, <see cref="Encoding.UTF8"/> is used.</param>
     /// <returns></returns>
-    public static string MemReadCString(this IUnicorn unicorn, ulong address, StringBuilder? stringBuilder = null)
+    /// <exception cref="OverflowException">No null terminator found after reading <paramref name="maxLength"/> bytes,
+    /// or <see cref="maxAddress"/> exceeded.</exception>
+    public static string MemReadCString(this IUnicorn unicorn, ulong address, int maxLength = 256,
+        ulong maxAddress = ulong.MaxValue, Encoding? encoding = null)
     {
-        stringBuilder ??= new StringBuilder();
+        if (maxLength <= 0)
+            throw new ArgumentOutOfRangeException(nameof(maxLength));
 
         const int bufferLength = 16;
-        Span<byte> buffer = stackalloc byte[bufferLength];
+        const int maxStackAlloc = 128;
 
-        while (true)
-        {
-            unicorn.MemRead(address, buffer, bufferLength);
-
-            for (var i = 0; i < bufferLength; i++)
-            {
-                if (buffer[i] != 0)
-                    continue;
-
-                if (i != 0)
-                    stringBuilder.Append(Encoding.ASCII.GetString(buffer[..i]));
-
-                return stringBuilder.ToString();
-            }
-
-            stringBuilder.Append(Encoding.ASCII.GetString(buffer));
-            address += bufferLength;
-        }
-    }
-
-    public static string MemReadEncodedCString(this IUnicorn unicorn, ulong address, Encoding? encoding = null)
-    {
         encoding ??= Encoding.UTF8;
 
-        const int bufferLength = 16;
         Span<byte> buffer = stackalloc byte[bufferLength];
-
-        var stringLength = 0;
+        var size = -bufferLength;
         var found = false;
 
-        while (true)
+        while (!found)
         {
-            unicorn.MemRead(address + (ulong)stringLength, buffer, bufferLength);
+            size += bufferLength;
 
-            for (var i = 0; i < bufferLength; i++)
+            if (size >= maxLength)
+                throw new OverflowException($"No null terminator found after reading {size} bytes.");
+
+            var currentAddress = address + (ulong)size;
+
+            if (currentAddress >= maxAddress)
+                throw new OverflowException("No null terminator found before reaching the maximum address.");
+
+            var toRead = (int) Math.Min(bufferLength, maxAddress - currentAddress);
+            unicorn.MemRead(currentAddress, buffer, (nuint)toRead);
+
+            for (var i = 0; i < toRead; i++)
             {
-                if (buffer[i] != 0)
-                    continue;
+                if (buffer[i] == 0)
+                {
+                    size += i;
+                    found = true;
 
-                stringLength += i;
-                found = true;
-
-                break;
+                    break;
+                }
             }
-
-            if (found)
-                break;
-
-            stringLength += bufferLength;
         }
 
-        if (stringLength <= bufferLength)
-            return encoding.GetString(buffer[..(stringLength + 1)]);
+        if (size <= bufferLength)
+            return encoding.GetString(buffer[..size]);
 
-        var targetArray = ArrayPool<byte>.Shared.Rent(stringLength);
-        unicorn.MemRead(address, targetArray, (nuint)stringLength);
-        var str = encoding.GetString(targetArray[..(stringLength + 1)]);
-        ArrayPool<byte>.Shared.Return(targetArray);
+        if (size >= maxStackAlloc)
+        {
+            var target = ArrayPool<byte>.Shared.Rent(size);
+            unicorn.MemRead(address, target, (nuint)size);
+            var ret = encoding.GetString(target, 0, size);
+            ArrayPool<byte>.Shared.Return(target);
 
-        return str;
+            return ret;
+        }
+        else
+        {
+            Span<byte> target = stackalloc byte[size];
+            unicorn.MemRead(address, target);
+
+            return encoding.GetString(target);
+        }
     }
 
+    /// <summary>
+    /// Reads a value of type <typeparamref name="T"/> from the emulated memory.
+    /// No byte order conversion is performed.
+    /// </summary>
+    /// <param name="unicorn">The Unicorn instance.</param>
+    /// <param name="address">The start address.</param>
+    /// <typeparam name="T">The type of value to read.</typeparam>
+    /// <returns>A value of type <typeparamref name="T"/> read from the emulated memory.</returns>
     public static unsafe T MemReadDirect<T>(this IUnicorn unicorn, ulong address) where T : unmanaged
     {
         var size = sizeof(T);
@@ -96,6 +102,14 @@ public static class UnicornExtensions
         return resultCast[0];
     }
 
+    /// <summary>
+    /// Reads a value of type <typeparamref name="T"/> from the emulated memory.
+    /// Performs byte order conversion between the hosting system and the emulated one.
+    /// </summary>
+    /// <param name="unicorn">The Unicorn instance.</param>
+    /// <param name="address">The start address.</param>
+    /// <typeparam name="T">The type of value to read.</typeparam>
+    /// <returns>A value of type <typeparamref name="T"/> read from the emulated memory.</returns>
     public static unsafe T MemReadSafe<T>(this IUnicorn unicorn, ulong address) where T : unmanaged
     {
         var localLe = BitConverter.IsLittleEndian;
@@ -112,5 +126,81 @@ public static class UnicornExtensions
         var resultCast = MemoryMarshal.Cast<byte, T>(result);
 
         return resultCast[0];
+    }
+
+    /// <summary>
+    /// Writes a value of type <typeparamref name="T"/> to the emulated memory.
+    /// No byte order conversion is performed so the data is written with the hosting system's byte order.
+    /// </summary>
+    /// <param name="unicorn">The Unicorn instance.</param>
+    /// <param name="address">The start address.</param>
+    /// <param name="value">The value.</param>
+    /// <typeparam name="T">The type of value to write.</typeparam>
+    public static unsafe void MemWriteDirect<T>(this IUnicorn unicorn, ulong address, T value)
+        where T : unmanaged
+    {
+        MemWriteDirect(unicorn, address, value, (nuint)sizeof(T));
+    }
+
+    /// <summary>
+    /// Writes a value of type <typeparamref name="T"/> to the emulated memory.
+    /// No byte order conversion is performed so the data is written with the hosting system's byte order.
+    /// </summary>
+    /// <param name="unicorn">The Unicorn instance.</param>
+    /// <param name="address">The start address.</param>
+    /// <param name="value">The value.</param>
+    /// <param name="targetSize">The maximum number of bytes to write.</param>
+    /// <typeparam name="T">The type of value to write.</typeparam>
+    public static void MemWriteDirect<T>(this IUnicorn unicorn, ulong address, T value, nuint targetSize)
+        where T : unmanaged
+    {
+        var valueSpan = MemoryMarshal.CreateReadOnlySpan(ref value, 1);
+        var bytes = MemoryMarshal.Cast<T, byte>(valueSpan);
+
+        unicorn.MemWrite(address, bytes, targetSize);
+    }
+
+    /// <summary>
+    /// Writes a value of type <typeparamref name="T"/> to the emulated memory.
+    /// Performs byte order conversion so the data is always written with the emulated system's byte order.
+    /// </summary>
+    /// <param name="unicorn">The Unicorn instance.</param>
+    /// <param name="address">The start address.</param>
+    /// <param name="value">The value.</param>
+    /// <typeparam name="T">The type of value to write.</typeparam>
+    public static unsafe void MemWriteSafe<T>(this IUnicorn unicorn, ulong address, T value)
+        where T : unmanaged
+    {
+        MemWriteSafe(unicorn, address, value, (nuint)sizeof(T));
+    }
+
+    /// <summary>
+    /// Writes a value of type <typeparamref name="T"/> to the emulated memory.
+    /// Performs byte order conversion so the data is always written with the emulated system's byte order.
+    /// </summary>
+    /// <param name="unicorn">The Unicorn instance.</param>
+    /// <param name="address">The start address.</param>
+    /// <param name="value">The value.</param>
+    /// <param name="targetSize">The maximum number of bytes to write.</param>
+    /// <typeparam name="T">The type of value to write.</typeparam>
+    public static unsafe void MemWriteSafe<T>(this IUnicorn unicorn, ulong address, T value, nuint targetSize)
+        where T : unmanaged
+    {
+        var localLe = BitConverter.IsLittleEndian;
+        var unicornLe = (unicorn.CurrentMode & (int)EngineMode.BigEndian) == 0;
+        var size = sizeof(T);
+
+        if (localLe == unicornLe || size == 1)
+        {
+            MemWriteDirect(unicorn, address, value);
+
+            return;
+        }
+
+        Span<byte> bytes = stackalloc byte[size];
+        MemoryMarshal.Write(bytes, ref value);
+
+        bytes.Reverse();
+        unicorn.MemWrite(address, bytes, targetSize);
     }
 }
